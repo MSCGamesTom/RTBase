0901baf8c26c1b08b96117a51dc31cb63a17c5d8
MSCGamesTom <134287460+MSCGamesTom@users.noreply.github.com>
2026-04-25 19:26:23 +0100

    Fixed replacing the base material with the Layered material

--- RTBase/SceneLoader.h
+++ RTBase/SceneLoader.h
@@ -184,6 +184,7 @@ void loadInstance(std::string sceneName, std::vector<Triangle>& meshTriangles, s
 		float extIOR = instance.material.find("coatingExtIOR").getValue(1.0f);
 		float thickness = instance.material.find("coatingThickness").getValue(0.0f);
 		material = new LayeredBSDF(base, sigmaa, thickness, intIOR, extIOR);
+		meshMaterials[meshMaterials.size() - 1] = material;
 	}
 	if (material == NULL)
 	{
